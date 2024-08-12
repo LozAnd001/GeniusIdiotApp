711483abf31c81956043e5fa85d0f178c6a02a7a
andre <andreyvl001@mail.ru>
2024-08-12 13:24:39 +0300

    Добавил возможность удаления вопроса

--- GeniyIdiotWinFormsApp/QuestionListForm.Designer.cs
+++ GeniyIdiotWinFormsApp/QuestionListForm.Designer.cs
@@ -31,6 +31,7 @@
             questionsDataGridView = new DataGridView();
             Column1 = new DataGridViewTextBoxColumn();
             Column2 = new DataGridViewTextBoxColumn();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)questionsDataGridView).BeginInit();
             SuspendLayout();
             // 
@@ -38,7 +39,7 @@
             // 
             questionsDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             questionsDataGridView.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2 });
-            questionsDataGridView.Location = new Point(12, 12);
+            questionsDataGridView.Location = new Point(12, 69);
             questionsDataGridView.Name = "questionsDataGridView";
             questionsDataGridView.RowHeadersWidth = 51;
             questionsDataGridView.Size = new Size(308, 426);
@@ -58,11 +59,22 @@
             Column2.Name = "Column2";
             Column2.Width = 125;
             // 
+            // button1
+            // 
+            button1.Location = new Point(256, 18);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Удалить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // QuestionListForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(328, 444);
+            ClientSize = new Size(386, 546);
+            Controls.Add(button1);
             Controls.Add(questionsDataGridView);
             Name = "QuestionListForm";
             Text = "QuestionListForm";
@@ -76,5 +88,6 @@
         private DataGridView questionsDataGridView;
         private DataGridViewTextBoxColumn Column1;
         private DataGridViewTextBoxColumn Column2;
+        private Button button1;
     }
 }
--- GeniyIdiotWinFormsApp/QuestionListForm.cs
+++ GeniyIdiotWinFormsApp/QuestionListForm.cs
@@ -21,10 +21,27 @@ namespace GeniusIdiotWinFormsApp
         private void QuestionListForm_Load(object sender, EventArgs e)
         {
             var questions = QuestionStorage.GetAll();
-            foreach(var question in questions)
+            foreach (var question in questions)
             {
                 questionsDataGridView.Rows.Add(question.Text, question.Answer);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var rows = questionsDataGridView.SelectedRows;
+            if(rows.Count == 0)
+            {
+                MessageBox.Show("Вы не выбради строку");
+                return;
+            }
+            var questionText = rows[0].Cells[0].Value.ToString();
+            var questionAnswer = Convert.ToInt32(rows[0].Cells[1].Value);
+            if(questionText != null)
+            {
+                QuestionStorage.Remove(new Question(questionText, questionAnswer));
+            }
+            Close();
         }
     }
 }
